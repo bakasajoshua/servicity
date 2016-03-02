9c14cae4876fed9daa91180d9a013d11d139b3c2
Joshua Bakasa <baksajoshua09@gmail.com>
2016-03-02 16:21:08 +0300

    initial commit

---
+++ application/modules/welcome/controllers/Welcome.php
@@ -0,0 +1,39 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Welcome extends MY_Controller {
+
+	/**
+	 * Index Page for this controller.
+	 *
+	 * Maps to the following URL
+	 * 		http://example.com/index.php/welcome
+	 *	- or -
+	 * 		http://example.com/index.php/welcome/index
+	 *	- or -
+	 * Since this controller is set as the default controller in
+	 * config/routes.php, it's displayed at http://example.com/
+	 *
+	 * So any other public methods not prefixed with an underscore will
+	 * map to /index.php/welcome/<method_name>
+	 * @see https://codeigniter.com/user_guide/general/urls.html
+	 */
+	public function index()
+	{
+		$this->load->view('welcome_message');
+	}
+
+	public function validate_email($email)
+	{
+		// Remove all illegal characters from email
+		$email = filter_var($email, FILTER_SANITIZE_EMAIL);
+
+		// Validate e-mail
+		if (!filter_var($email, FILTER_VALIDATE_EMAIL) === false) {
+			$text = $email." is a valid email address";
+		} else {
+			$text = $email." is not a valid email address";
+		}
+		echo json_encode($text);
+	}
+}
